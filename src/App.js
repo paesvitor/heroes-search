3e5ff0a9bc7ecddf962f4106cecdf2b32390dc5c
Vitor Paes <paesvitor@outlook.com>
2020-03-06 15:14:56 -0300

    chore: Add theme configuration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Provider as StoreProvider} from 'react-redux';
 import Router from 'router';
+import {ThemeProvider} from 'styled-components';
+import theme from 'resources/theme';
 import store from './store';
 
 function App() {
   return (
     <StoreProvider store={store}>
-      <Router />
+      <ThemeProvider theme={theme}>
+        <Router />
+      </ThemeProvider>
     </StoreProvider>
   );
 }
---
+++ src/resources/theme.js
@@ -0,0 +1,9 @@
+export default {
+  palette: {
+    primary: {
+      dark: '#FF0000',
+      main: '#FA7C7C',
+      light: '#FDECEC',
+    },
+  },
+};
