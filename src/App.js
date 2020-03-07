9dff7986d3d4b53519102d124e7f654a0fa2542e
Vitor Paes <paesvitor@outlook.com>
2020-03-07 17:33:32 -0300

    feat: Add GlobalStyles to App

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Router from 'router';
 import {ThemeProvider} from 'styled-components';
 import theme from 'resources/theme';
 import store from './store';
+import GlobalStyles from './resources/globalStyles';
 
 function App() {
   return (
     <StoreProvider store={store}>
       <ThemeProvider theme={theme}>
+        <GlobalStyles />
         <Router />
       </ThemeProvider>
     </StoreProvider>
