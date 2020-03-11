eeed219ad7031dc8eef206fdc73793492ecb51e3
Vitor Paes <paesvitor@outlook.com>
2020-03-11 15:58:56 -0300

    feat: Add favorite icon to Icon component

--- src/components/common/Icon/index.js
+++ src/components/common/Icon/index.js
@@ -4,12 +4,14 @@ import iconSearch from 'resources/icons/ic_busca_menor.svg';
 import iconHero from 'resources/icons/ic_heroi.svg';
 import iconComics from 'resources/icons/ic_quadrinhos.svg';
 import iconTrailer from 'resources/icons/ic_trailer.svg';
+import iconFavorite from 'resources/icons/favorito_01.svg';
 
 const icons = {
   search: iconSearch,
   hero: iconHero,
   comics: iconComics,
   trailer: iconTrailer,
+  favorite: iconFavorite,
 };
 
 function Icon({name}) {
@@ -17,7 +19,8 @@ function Icon({name}) {
 }
 
 Icon.propTypes = {
-  name: PropTypes.oneOf(['search', 'hero', 'comics', 'trailer']).isRequired,
+  name: PropTypes.oneOf(['search', 'hero', 'comics', 'trailer', 'favorite'])
+    .isRequired,
 };
 
 export default Icon;
